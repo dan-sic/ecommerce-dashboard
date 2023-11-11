91f3940db788856ddf6c1ceee7238c90a94eef92
Daniel Sicinski <daniel.sicinski@netguru.com>
2023-11-12 00:07:10 +0100

    feat: add pm2 ecosystem config file

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-process-env */
+require("dotenv").config()
+
+module.exports = {
+  apps: [
+    {
+      script: "npm start",
+    },
+    {
+      script: "node s3.js",
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: "key.pem",
+      user: process.env.VPS_USER,
+      host: process.env.VPS_HOST,
+      port: process.env.VPS_PORT,
+      ref: "origin/main",
+      repo: "git@github.com:dan-sic/ecommerce-dashboard.git",
+      path: process.env.VPS_PATH,
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && npm install && npm run build && npx prisma migrate deploy && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+}
